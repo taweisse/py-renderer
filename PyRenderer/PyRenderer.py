cd8784a1b60eea1f99586b57b634c94d1e6e3f9b
taweisse <taweisse@gmail.com>
2017-11-10 18:23:47 -0500

    Working on mouse click and drag events to rotage th model...

--- PyRenderer/PyRenderer.py
+++ PyRenderer/PyRenderer.py
@@ -4,8 +4,15 @@ import math
 from Object import Object
 
 pygame.init()
-screen = pygame.display.set_mode((600,600))
-cx, cy = 300, 300
+
+# The resolution of the output window.
+resX, resY = 1280, 720
+screen = pygame.display.set_mode((resX, resY))
+
+# Find the middle of the output window.
+cx = int(resX/2)
+cy = int(resY/2)
+
 pygame.display.set_caption('3D Renderer')
 clock = pygame.time.Clock()
 
@@ -15,6 +22,10 @@ model.test()
 model.loadModel("teapot.obj")
 print(model.verticies)
 
+# Flag to tell if the user is clicking and dragging.
+# If they are, we need to rotate the model accordingly.
+mouseDrag = False;
+
 while 1:
     dt = clock.tick()/1000
 
@@ -22,15 +33,24 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouseDrag = True
+            
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                mouseDrag = False
             
         print(event)
+        print(mouseDrag)
 
     screen.fill((255,255,255))
 
     for x,y,z in model.verticies:
         z+=5
-        f = 300/z
+        f = cy/z
         x,y = x*f,y*f
-        pygame.draw.circle(screen, (0,0,0),(cx+int(x),cy+int(y)), 1)
+        pygame.draw.circle(screen, (0,0,0),(cx-int(x),cy-int(y)), 1)
 
     pygame.display.update()
