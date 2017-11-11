5112f015fec11dbfe758b6fdb9e4dcfd08f3ade1
taweisse <taweisse@gmail.com>
2017-11-10 20:23:50 -0500

    commit

--- PyRenderer/PyRenderer.py
+++ PyRenderer/PyRenderer.py
@@ -1,18 +1,19 @@
 import pygame
+import numpy
 import sys
 import math
 from Object import Object
 
 pygame.init()
-screen = pygame.display.set_mode((600,600))
-cx, cy = 300, 300
+screen = pygame.display.set_mode((1280,720))
+cx, cy = 640, 360
 pygame.display.set_caption('3D Renderer')
 clock = pygame.time.Clock()
 
 # Create an object.
 model = Object()
 model.test()
-model.loadModel("teapot.obj")
+model.loadModel("Test Objects/teapot.obj")
 print(model.verticies)
 
 while 1:
@@ -29,7 +30,7 @@ while 1:
 
     for x,y,z in model.verticies:
         z+=5
-        f = 300/z
+        f = cx/z
         x,y = x*f,y*f
         pygame.draw.circle(screen, (0,0,0),(cx+int(x),cy+int(y)), 1)
 
